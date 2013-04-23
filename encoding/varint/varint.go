f84aa98af776f83a2e485bcb345377301ffb236f
Yves Junqueira <yves.junqueira@gmail.com>
2013-04-24 01:03:18 +0200

    Add the option to use BigEndian for varints.
    Makes the library compatible with BitMessage too: see www.bitmessage.org.

--- encoding/varint/varint.go
+++ encoding/varint/varint.go
@@ -7,6 +7,8 @@ import (
 
 const MaxBufferSize = 9
 
+var ByteOrder binary.ByteOrder = binary.LittleEndian
+
 func PutVarInt(buf []byte, value uint64) int {
 
 	i := 0
@@ -18,17 +20,17 @@ func PutVarInt(buf []byte, value uint64) int {
 	case value <= uint64(65535):
 		buf[0] = 0xfd
 
-		binary.LittleEndian.PutUint16(buf[1:3], uint16(value))
+		ByteOrder.PutUint16(buf[1:3], uint16(value))
 		i = 3
 	case value <= uint64(4294967295):
 		buf[0] = 0xfe
 
-		binary.LittleEndian.PutUint32(buf[1:5], uint32(value))
+		ByteOrder.PutUint32(buf[1:5], uint32(value))
 		i = 5
 	default:
 		buf[0] = 0xff
 
-		binary.LittleEndian.PutUint64(buf[1:9], value)
+		ByteOrder.PutUint64(buf[1:9], value)
 		i = 9
 	}
 
@@ -66,21 +68,21 @@ func ReadVarInt(r io.Reader) (uint64, int, error) {
 			return 0, i, err
 		}
 
-		value = uint64(binary.LittleEndian.Uint16(buf))
+		value = uint64(ByteOrder.Uint16(buf))
 		i = 3
 	case 0xfe:
 		_, err := io.ReadFull(r, buf[0:4])
 		if err != nil {
 			return 0, i, err
 		}
-		value = uint64(binary.LittleEndian.Uint32(buf))
+		value = uint64(ByteOrder.Uint32(buf))
 		i = 5
 	case 0xff:
 		_, err := io.ReadFull(r, buf[0:8])
 		if err != nil {
 			return 0, i, err
 		}
-		value = binary.LittleEndian.Uint64(buf)
+		value = ByteOrder.Uint64(buf)
 		i = 9
 	}
 
@@ -97,13 +99,13 @@ func VarInt(buf []byte) (uint64, int) {
 		value = uint64(buf[0])
 		i = 1
 	case 0xfd:
-		value = uint64(binary.LittleEndian.Uint16(buf[1:3]))
+		value = uint64(ByteOrder.Uint16(buf[1:3]))
 		i = 3
 	case 0xfe:
-		value = uint64(binary.LittleEndian.Uint32(buf[1:5]))
+		value = uint64(ByteOrder.Uint32(buf[1:5]))
 		i = 5
 	case 0xff:
-		value = binary.LittleEndian.Uint64(buf[1:9])
+		value = ByteOrder.Uint64(buf[1:9])
 		i = 9
 	}
 
